73e6abf66b4fc4bba9e5841466b61ddac0ef0c9d
thednp <dnp.digital@gmail.com>
2022-03-07 11:24:15 +0200

    Delete rollup.config.d.ts

--- rollup.config.d.ts
+++
@@ -1,9 +0,0 @@
-declare var _default: {
-    input: string;
-    output: {
-        file: string;
-        format: string | undefined;
-    };
-    plugins: import("rollup").Plugin[];
-}[];
-export default _default;
